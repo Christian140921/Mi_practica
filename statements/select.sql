24e077053c9f00cbaf57254a93b87671c9183e35
cris vargas <cristianvargas6322@gmail.com>
2025-10-09 10:23:01 -0500

    nuevos cambios al sistema

---
+++ statements/select.sql
@@ -0,0 +1,8 @@
+--Esto es una consulta simple a una tabla de una bd
+SELECT * FROM usuarios; 
+--Esto es una consulta que selecciona columnas específicas
+SELECT user_id, name FROM usuarios;
+--Nuevamente es una consulta simple
+SELECT * FROM pedidos;
+--Nuevamente es una consulta que selecciona columnas específicas
+SELECT producto FROM pedidos
