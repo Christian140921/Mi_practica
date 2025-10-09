eedff4b3c1612df7ac2cb164de050533272faa11
cris vargas <cristianvargas6322@gmail.com>
2025-10-09 11:35:47 -0500

    cambios en sql

--- statements/02_disntic.sql
+++ statements/02_disntic.sql
@@ -1,2 +1,4 @@
 --Selecciona todos los registros únicos de la tabla usuarios (sin duplicados)
 SELECT distinct * FROM usuarios;
+--selecciona datos específicos sin duplicados
+SELECT distinct name, age FROM usuarios;
